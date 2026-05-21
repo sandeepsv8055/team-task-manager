c23ffce016fc1920d48dc9a1241c700b7b354440
Richa Singh <143858545+sandeepsv8055@users.noreply.github.com>
2026-05-21 18:18:48 +0530

    commit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://team-task-manager-kmic.onrender.com/api'
+  baseURL: 'https://task-manager-backend-adnh.onrender.com'
 });
 
 API.interceptors.request.use((req) => {
